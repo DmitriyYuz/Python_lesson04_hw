eac174d87ebe63c583188ca8eee4b3d4d6443d92
DmitriyYuz <visionsslip@gmail.com>
2020-02-05 01:53:02 +0300

    L04

---
+++ L04_hw01.py
@@ -0,0 +1,11 @@
+
+def sal():
+    try:
+        time = float(input('Выработка в часах: '))
+        salary = int(input('Ставка в час: '))
+        bonus = int(input('Премия: '))
+        res = time * salary + bonus
+        print(f'заработная плата сотрудника:  {res}')
+    except ValueError:
+        return print('Not a number')
+sal()
